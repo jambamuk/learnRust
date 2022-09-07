ad7a7fb66da021c2cfb0757829140e62b513da1a
Jameel Mukadam <jambamuk@gmail.com>
2022-09-07 10:26:24 +0200

    continuation of rust book laerning

--- calculate-shape/src/main.rs
+++ calculate-shape/src/main.rs
@@ -1,19 +1,33 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn square(size: u32) -> Self{
+        Self { width: size , height: size }
+    }
+    fn area(&self) -> u32 {
+        Self.width * self.height
+    }
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        if self.area() >= rectangle.area() {
+            true
+        } else {
+            false
+        }
+    }
+}
 fn main() {
-    let rectangle = Rectangle {
+    let rectangle1 = Rectangle {
         width: 30,
         height: 60,
     };
-
-    println!(
-        "The area of the rectangle is {} pixels.",
-        area(&rectangle)
-        );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    let rectangle2 = Rectangle {
+        width: 40,
+        ..rectangle1
+    };
+    let square = Rectangle::square(40);
+    println!("square {:?}", square);
 }
